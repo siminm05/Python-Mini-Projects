183b93997a765d27c699a71d7c48a31266f1146b
Simin Mulla <siminm1905@gmail.com>
2026-07-29 18:00:21 +0530

    Improve user prompt and feedback messages
    Entry for multiple user guesses till the correct answer is reached

--- 2. NumberGuessingGame.py
+++ 2. NumberGuessingGame.py
@@ -6,11 +6,16 @@ import random
 
 correct_answer = random.randint(1, 100)
 #print("Correct answer:", correct_answer)
-user_guess = int(input("Enter a num [1 to 100]: "))
+user_guess = int(input("Enter your guess [1 to 100]: "))
+
+while (user_guess != correct_answer):
+    if user_guess < correct_answer:
+        print("Too low!")
+    else:
+        print("Too high!")
+
+    user_guess = int(input("Enter your guess again: "))
 
 if user_guess == correct_answer:
-    print("Spot on!")
-elif user_guess < correct_answer:
-    print("Too low!")
-else:
-    print("Too high!")
+   print("Spot on!")
+

2cadc0a0df56ccb964385f18ef485a2ef833b84a
Simin Mulla <siminm1905@gmail.com>
2026-07-30 17:47:29 +0530

    Refactor password generation to include uppercase letters

--- 03_Password_Generator/password_generator.py
+++ 03_Password_Generator/password_generator.py
@@ -3,11 +3,10 @@ import string
 
 user_pw_length = int(input("Enter password length: "))
 #letters = "abcdefghijklmnopqrstuvwxyz"
-lower_letters = string.ascii_lowercase
-i = 0
+letters = string.ascii_letters
+#i = 0
 password = ""
-for i in range(user_pw_length):
-    password = password + random.choice(lower_letters)
-    i+=1
+for _ in range(user_pw_length):
+    password = password + random.choice(letters)
 
 print(password)

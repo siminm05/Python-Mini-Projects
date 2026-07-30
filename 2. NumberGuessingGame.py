1f4ac9bc43598b0a29e0096939eb6e3fe6ec940e
Simin Mulla <siminm1905@gmail.com>
2026-07-30 16:52:50 +0530

    Add difficulty levels and adjust guess limits

--- 2. NumberGuessingGame.py
+++ 2. NumberGuessingGame.py
@@ -4,13 +4,30 @@ Number Guessing Game
 
 import random
 
+numOfChances = 0
+
+try:
+    user_difficulty = int(input("1. Easy\n2. Medium\n3. Hard\nSelect Difficulty: "))
+
+    if user_difficulty==1:
+        numOfChances = 10
+    elif user_difficulty==2:
+        numOfChances = 7
+    elif user_difficulty ==3:
+        numOfChances = 5
+    else:
+        print("Invalid Input")
+
+except ValueError:
+    print("Enter a Number [1 to 3]")
+
 correct_answer = random.randint(1, 100)
 #print("Correct answer:", correct_answer)
 
 user_guess = 0
 count = 0
 
-while (user_guess != correct_answer) and (count < 5):
+while (user_guess != correct_answer) and (count < numOfChances):
     try:
         user_guess = int(input("Enter your guess [1 to 100]: "))
 
@@ -22,14 +39,14 @@ while (user_guess != correct_answer) and (count < 5):
             print("Too high!")
 
         count += 1
+        #print("Count: ", count)
 
-        if count == 5 and user_guess != correct_answer:
+        if count == numOfChances and user_guess != correct_answer:
             print("Game Over. You are out of guesses.")
             break
 
-        # print("Count: ",count)
-        if count < 5 and user_guess != correct_answer:
-            print("Guesses remaining:", 5 - count)
+        if count < numOfChances and user_guess != correct_answer:
+            print("Guesses remaining:", numOfChances - count)
 
     except ValueError:
         print("Enter Int only")

26c7939b0eb3e960285a28fb4194f6e9068e2cd0
Simin Mulla <siminm1905@gmail.com>
2026-07-30 16:31:39 +0530

    Modify guessing logic and increase attempts to 5

--- 2. NumberGuessingGame.py
+++ 2. NumberGuessingGame.py
@@ -6,23 +6,30 @@ import random
 
 correct_answer = random.randint(1, 100)
 #print("Correct answer:", correct_answer)
-user_guess = int(input("Enter your guess [1 to 100]: "))
+
+user_guess = 0
 count = 0
 
-while (user_guess != correct_answer) and (count < 4):
-    if user_guess < correct_answer:
-        print("Too low!")
-    else:
-        print("Too high!")
+while (user_guess != correct_answer) and (count < 5):
+    try:
+        user_guess = int(input("Enter your guess [1 to 100]: "))
 
-    count += 1
-    print("Guesses remaining:", 5-count)
+        if user_guess < correct_answer:
+            print("Too low!")
+        elif user_guess == correct_answer:
+            print("Spot on!")
+        else:
+            print("Too high!")
 
-    user_guess = int(input("Enter your guess again: "))
+        count += 1
 
-    if count == 4 and user_guess!=correct_answer:
-        print("Game Over. You are out of guesses.")
+        if count == 5 and user_guess != correct_answer:
+            print("Game Over. You are out of guesses.")
+            break
 
-if user_guess == correct_answer:
-   print("Spot on!")
+        # print("Count: ",count)
+        if count < 5 and user_guess != correct_answer:
+            print("Guesses remaining:", 5 - count)
 
+    except ValueError:
+        print("Enter Int only")

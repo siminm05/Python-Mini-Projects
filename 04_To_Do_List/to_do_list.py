3cb0177286241fe466fa7b147fa8e0147eaa71e4
Simin Mulla <siminm1905@gmail.com>
2026-08-02 21:08:04 +0530

    Add task completion status and menu
    Refactor task management functions and menu

--- 04_To_Do_List/to_do_list.py
+++ 04_To_Do_List/to_do_list.py
@@ -1,11 +1,40 @@
 tasks = []
 
+def menu(user_choice):
+    if user_choice == 1:
+        user_task = input("Enter Task: ")
+        tasks.append({
+            "task": user_task,
+            "completed": False})
+
+    elif user_choice == 2:
+        if len(tasks) == 0:
+            print("No tasks to do")
+        else:
+            view_tasks()
+
+    elif user_choice == 3:
+        if len(tasks) == 0:
+            print("No tasks to be deleted")
+        else:
+            delete_task()
+
+    elif user_choice == 4:
+        if len(tasks) == 0:
+            print("No tasks to be deleted")
+        else:
+            complete_task()
+
+    else:
+        print("Enter number")
 
 def view_tasks():
     i = 1
-    print("Current Tasks: ")
     for task in tasks:
-        print(i, ". ", task, sep="")
+        if task["completed"]:
+            print(i, ". ✓ ", task["task"], sep="")
+        else:
+            print(i, ". ✗ ", task["task"], sep="")
         i += 1
 
 
@@ -16,44 +45,48 @@ def delete_task():
             print("\nWhich task would you like to delete? ")
             delete_index = int(input("Enter the index next to the task: "))
             if 0 < delete_index <= len(tasks):
-                print("Task '", tasks[delete_index - 1], "' has been deleted", sep="")
+                print("Task '", tasks[delete_index - 1]["task"], "' has been deleted", sep="")
                 tasks.pop(delete_index - 1)
+                return
             else:
                 print("Invalid Task number\n")
         except ValueError:
             print("\nInvalid Input type. Enter task index\n")
 
-while True:
-    try:
-        print("\n1. Add Task\n2. View Tasks\n3. Delete Task\n4. Exit")
-        user_choice = int(input("Choose an option [1 to 4]: "))
-        print("")
-
-        if user_choice == 1:
-            user_task = input("Enter Task: ")
-            tasks.append(user_task)
-            continue
-
-        elif user_choice == 2:
-            if len(tasks) == 0:
-                print("No tasks to do")
+def complete_task():
+    while True:
+        try:
+            view_tasks()
+            print("\nWhich task would to like to mark 'Completed'?")
+            complete_index = int(input("Enter the index next to the task: "))
+            if 0 < complete_index <= len(tasks):
+                # if tasks[complete_index - 1]["completed"] == False:
+                if not tasks[complete_index - 1]["completed"]:
+                    print("Task' ", tasks[complete_index-1]["task"], "' has been marked completed", sep="")
+                    tasks[complete_index-1]["completed"] = True
+                    return
+                else:
+                    print("Task has already been completed")
             else:
-                view_tasks()
-            continue
+                print("Invalid Task Number\n")
+        except ValueError:
+            print("\nInvalid input type. Enter task index\n")
 
-        elif user_choice == 3:
-            if len(tasks) == 0:
-                print("No tasks to be deleted")
+
+def main():
+    while True:
+        try:
+            print("\n1. Add Task\n2. View Tasks\n3. Delete Task\n4. Complete Task\n5. Exit")
+            user_choice = int(input("Choose an option: "))
+            print("")
+            if user_choice == 5:
+                print("Thank you")
+                break
             else:
-                delete_task()
-            continue
+                menu(user_choice)
 
-        elif user_choice == 4:
-            print("Thank you")
-            break
+        except ValueError:
+            print("Invalid value type. Enter number")
 
-        else:
-            print("Enter number [1 to 4]")
 
-    except ValueError:
-        print("Invalid value type. Enter number [1 to 4]")
+main()

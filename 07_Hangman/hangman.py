99dca6e1df66856b3c4f849cfe14339ae5231863
Simin Mulla <siminm1905@gmail.com>
2026-08-07 19:30:47 +0530

    Refactor hangman game with improved input validation
    Refactor hangman game logic to improve user input handling and game flow.

--- 07_Hangman/hangman.py
+++ 07_Hangman/hangman.py
@@ -1,35 +1,38 @@
-word = []
-empty_spaces = []
-user_guess_list = []
+def main():
+    word = "hangman"
+    empty_spaces = []
+    user_guess_list = []
+    chances = 5
 
-answer = "hangman"
+    for _ in word:
+        empty_spaces.append("__")
 
-i = 0
-while i != len(word):
-    empty_spaces.append("__")
-    i += 1
-
-print(*empty_spaces)
-chances = 5
-while "__" in empty_spaces:
-    if chances != 0:
-        user_guess = input("Enter a guess: ").lower().strip()
-        if user_guess not in user_guess_list:
-            user_guess_list.append(user_guess)
-            if user_guess in word:
-                for i in range(len(word)):
-                    if word[i].lower() == user_guess:
-                        empty_spaces[i] = user_guess
-                print(*empty_spaces)
-            else:
-                print("Incorrect guess")
-                print(*empty_spaces)
-                chances -= 1
-            print("Chances left:", chances)
+    print(*empty_spaces)
+    while chances > 0 and "__" in empty_spaces:
+        user_guess = input("\nEnter a guess: ").lower().strip()
+        if not user_guess.isalpha():
+            print("\nEnter alphabets only")
+            continue
+        if len(user_guess) != 1:
+            print("\nEnter a single alphabet")
+            continue
+        if user_guess in user_guess_list:
+            print("\nAlready guessed the letter")
+            continue
+        user_guess_list.append(user_guess)
+        if user_guess in word:
+            for i in range(len(word)):
+                if word[i].lower() == user_guess:
+                    empty_spaces[i] = user_guess
+            print(*empty_spaces)
         else:
-            print("Already guessed the letter:", user_guess)
-    elif chances == 0 and "__" in empty_spaces:
-        print("You Lost")
-        break
+            print("\nIncorrect guess")
+            print(*empty_spaces)
+            chances -= 1
+        print("Chances left:", chances)
+    if chances == 0:
+        print("\nYou Lost")
     if "__" not in empty_spaces:
-        print("You Won")
+        print("\nYou Won")
+
+main()

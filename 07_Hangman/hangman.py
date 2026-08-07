788082803c3b17c16040c104097780cd92d7883f
Simin Mulla <siminm1905@gmail.com>
2026-08-07 20:02:34 +0530

    Refactor hangman game to use random word selection

--- 07_Hangman/hangman.py
+++ 07_Hangman/hangman.py
@@ -1,37 +1,45 @@
+import random
+
+
 def main():
-    word = "hangman"
+    words = ["Elephant", "Chocolate", "Mountain", "Umbrella", "Adventure",
+            "Basketball", "Butterfly", "Strawberry", "Television"]
     empty_spaces = []
     user_guess_list = []
     chances = 5
 
-    for _ in word:
+    answer = random.choice(words)
+
+    for _ in answer:
         empty_spaces.append("__")
 
-    print(*empty_spaces)
+    print("\n", *empty_spaces)
     while chances > 0 and "__" in empty_spaces:
-        user_guess = input("\nEnter a guess: ").lower().strip()
+        user_guess = input("Enter a guess: ").lower().strip()
         if not user_guess.isalpha():
-            print("\nEnter alphabets only")
+            print("Enter alphabets only\n")
             continue
         if len(user_guess) != 1:
-            print("\nEnter a single alphabet")
+            print("Enter a single alphabet\n")
             continue
         if user_guess in user_guess_list:
-            print("\nAlready guessed the letter")
+            print("Already guessed the letter\n")
             continue
         user_guess_list.append(user_guess)
-        if user_guess in word:
-            for i in range(len(word)):
-                if word[i].lower() == user_guess:
+        if user_guess in answer.lower():
+            for i in range(len(answer)):
+                if answer[i].lower() == user_guess:
                     empty_spaces[i] = user_guess
             print(*empty_spaces)
         else:
-            print("\nIncorrect guess")
+            print("Incorrect guess\n")
             print(*empty_spaces)
             chances -= 1
         print("Chances left:", chances)
+        print("Guessed letters:", *user_guess_list)
     if chances == 0:
         print("\nYou Lost")
+        print("Answer:", answer)
     if "__" not in empty_spaces:
         print("\nYou Won")
 
